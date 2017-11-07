ba68881266e89b44eba06700c3ec7074bcc18cdf
Neena van de Water <nkuriakose@hotmail.com>
2017-11-07 17:38:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
   array.join(" and ")
 end
 
-# def oxford_comma(array)
-#   array.join(" , ") + " and " + array[ -1]
-# end
+def oxford_comma(array)
+  array.to_sentence
+end
