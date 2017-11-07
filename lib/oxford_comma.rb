b3cb6599674643f33fe96f7042888876a2e390b2
Neena van de Water <nkuriakose@hotmail.com>
2017-11-07 17:52:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,3 +9,9 @@ end
 def oxford_comma(array)
   array[0..1].join(", ") + ", and " + array[ -1]
 end
+
+def oxford_comma(array)
+  if array.length > 3
+     array.join(", ") + ", and " + array[ -1]
+  end  
+end
