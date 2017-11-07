e38efa737de14b38db93d5460f8a42cf358ca1b9
Neena van de Water <nkuriakose@hotmail.com>
2017-11-07 18:38:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,17 @@
-# require 'pry'
-#
-# binding.pry
-def oxford_comma(array)
-  array.join
-end
-
-def oxford_comma(array)
-  array.join(" and ")
-end
-
-def oxford_comma(array)
-  array[0..1].join(", ") + ", and " + array[ -1]
-end
+# # require 'pry'
+# #
+# # binding.pry
+# def oxford_comma(array)
+#   array.join
+# end
+# 
+# def oxford_comma(array)
+#   array.join(" and ")
+# end
+# 
+# def oxford_comma(array)
+#   array[0..1].join(", ") + ", and " + array[ -1]
+# end
 
 def oxford_comma(array)
   if array.length == 1  
