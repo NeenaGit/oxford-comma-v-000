87a62bcd4afc7242ef6a84334d9f2958442cc4eb
Neena van de Water <nkuriakose@hotmail.com>
2017-11-07 18:38:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,25 +4,23 @@
 # def oxford_comma(array)
 #   array.join
 # end
-# 
+#
 # def oxford_comma(array)
 #   array.join(" and ")
 # end
-# 
+#
 # def oxford_comma(array)
 #   array[0..1].join(", ") + ", and " + array[ -1]
 # end
 
 def oxford_comma(array)
-  if array.length == 1  
+  if array.length == 1
     array.join
-  elsif array.length == 2  
+  elsif array.length == 2
     array.join(" and ")
-  elsif array.length == 3 
+  elsif array.length == 3
    array[0..1].join(", ") + ", and " + array[ -1]
   else array.length > 3
     array[0..-2].join(", ") + ", and " + array[ -1]
   end
 end
-
-
