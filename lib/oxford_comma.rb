72b78aeb1e84fd6aa3cb5587c7580d351b71bf91
Neena van de Water <nkuriakose@hotmail.com>
2017-11-07 17:41:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,13 @@
 def oxford_comma(array)
   array.join
-end
-
-def oxford_comma(array)
   array.join(" and ")
-end
-
-def oxford_comma(array)
   array.join(" , ") + " and " + array[ -1]
 end
+
+# def oxford_comma(array)
+#   array.join(" and ")
+# end
+# 
+# def oxford_comma(array)
+#   array.join(" , ") + " and " + array[ -1]
+# end
